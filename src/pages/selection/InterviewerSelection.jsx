152e93366a0bb81c1f085db18302216c2c3fc65e
Lee Chanu <125248552+I-Chanwoo@users.noreply.github.com>
2025-05-18 04:18:38 +0000

    Feat: 임시푸터 삭제, 채점 요소 비가시화

--- src/pages/selection/InterviewerSelection.jsx
+++ src/pages/selection/InterviewerSelection.jsx
@@ -122,25 +122,6 @@ const InterviewerSelection = () => {
           </div>
         </div>
       </main>
-
-      <div className="divider-line"></div>
-
-      <footer className="footer-area">
-        <hr className="footer-divider" />
-        <div className="footer-info">
-          <div className="footer-links">
-            <span>슈니첼</span>
-            <span>이용약관</span>
-            <span className="highlight">개인정보처리방침</span>
-            <span className="highlight">임시저장</span>
-          </div>
-          <div className="footer-desc">
-            (주)슈니첼 | 사업자등록번호 000-00-00000 | 통신판매업 신고번호 2025-서울노원-2025<br />
-            02-0000-0000 | official@swu | 서울특별시 노원구 화랑로 621 (슈니첼)<br />
-            © SWU Corp. All Rights Reserved.
-          </div>
-        </div>
-      </footer>
     </div>
   );
 };
--- src/pages/selection/PersonaSelection.jsx
+++ src/pages/selection/PersonaSelection.jsx
@@ -38,7 +38,7 @@ const PersonaSelection = () => {
                 </div>
                 <div className="body-14-regular persona-desc">{item.description}</div>
                 <div className="caption-14-regular persona-questions">
-                  {item.questions}
+                  <span style={{display:"none"}}>{item.questions}</span>
                 </div>
               </div>
             </div>
@@ -49,24 +49,6 @@ const PersonaSelection = () => {
           <button className="start-button small-button">면접 시작하기</button>
         </div>
       </main>
-
-      <div className="divider-line"></div>
-      <footer className="footer-area">
-        <hr className="footer-divider" />
-        <div className="footer-info">
-          <div className="footer-links">
-            <span>슈니첼</span>
-            <span>이용약관</span>
-            <span className="highlight">개인정보처리방침</span>
-            <span className="highlight">임시저장</span>
-          </div>
-          <div className="footer-desc">
-            (주)슈니첼 | 사업자등록번호 000-00-00000 | 통신판매업 신고번호 2025-서울노원-2025<br />
-            02-0000-0000 | official@swu | 서울특별시 노원구 화랑로 621 (슈니첼)<br />
-            © SWU Corp. All Rights Reserved.
-          </div>
-        </div>
-      </footer>
     </div>
   );
 };
