f266a510c4702b42024c31896b90f617606161d2
I-Chanwoo <chanuel0110@gmail.com>
2025-05-19 22:04:42 +0900

    Feat: 라우터 변경 및 인터뷰 복구

--- src/router.js
+++ src/router.js
@@ -3,8 +3,12 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-import InterviewSelfCheck from "./pages/interview/InterviewSelfCheck.jsx";
 import AIMock from "./pages/aiMock/AIMock.jsx";
+import PersonaSelection from './pages/selection/PersonaSelection.jsx';
+import InterviewerSelection from './pages/selection/InterviewerSelection.jsx';
+import SelfCheck from "./pages/selfCheck/SelfCheck.jsx";
+import FaceCheck from "./pages/selfCheck/FaceCheck.jsx";
+import SoundCheck from "./pages/selfCheck/SoundCheck.jsx";
 import Interview from "./pages/interview/Interview.jsx";
 import ReportMain from './pages/resultReport/ReportMain';
 import ReportNonverbal from './pages/resultReport/ReportNonverbal';
@@ -22,9 +26,14 @@ const RouterConfig = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/self-check" element={<InterviewSelfCheck />} />
+      
       <Route path="/ai-mock" element={<AIMock />} />
       <Route path="/interview" element={<Interview />} />
+
+      <Route path="/self-check" element={<SelfCheck />} />
+      <Route path="/self-check/face-check" element={<FaceCheck />} />
+      <Route path="/self-check/sound-check" element={<SoundCheck />} />
+
       <Route path="/result" element={<ReportMain />} />
       <Route path="/result/nonverbal" element={<ReportNonverbal />} />
       <Route path="/result/delivery" element={<ReportDelivery />} />
