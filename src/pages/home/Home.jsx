c75dc7550a1f04740662ce3fdc29ede622678860
I-Chanwoo <chanuel0110@gmail.com>
2025-05-19 22:02:51 +0900

    Chore: AI Mock 이미지 크기 조정 및 안쓰는 컴포넌트 삭제

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,31 +6,28 @@
  * @lastModified 2025-04-03
 **/
 
-import React, {useState, useEffect} from 'react';
+import React
+// , {useState, useEffect} 
+from 'react';
 import { Container } from "@mui/material";
 import NavbarComponent from '../../components/Navbar'
-// import { AnimatedLandingText } from "../../components/AnimatedLandingText"
 import "./Home.css"
-import MainBanner from "./MainBanner.jsx"
-import ServiceIntro from "./ServiceIntro.jsx"
-import KeyFeatures from "./KeyFeatures.jsx"
-import CallToAction from "./CallToAction.jsx"
 
 function Home() {
   /**
    * @state {number} scrollY - 스크롤 위치
    */
-  const [scrollY, setScrollY] = useState(0);
+  // const [scrollY, setScrollY] = useState(0);
   
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     setScrollY(window.scrollY);
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
 
   return (
     <Container maxWidth={false}
@@ -40,12 +37,8 @@ function Home() {
         padding: "0 0",
         overflow: "hidden"
       }}>
-      {/* <AnimatedLandingText /> */}
       <NavbarComponent />
-      <MainBanner scrollY={scrollY} />
-      <ServiceIntro scrollY={scrollY}/>
-      <KeyFeatures scrollY={scrollY}/>
-      <CallToAction scrollY={scrollY}/>
+
       
       
     </Container>
