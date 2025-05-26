144692c6b9e11c48551f9a9d901f797018eea101
I-Chanwoo <chanuel0110@gmail.com>
2025-05-26 23:08:29 +0900

    Chore: css 수정

--- src/pages/home/SectionIntro.jsx
+++ src/pages/home/SectionIntro.jsx
@@ -27,11 +27,9 @@ export default function SectionIntro() {
       <p className="body-16-medium" style={{ marginTop:"0px", marginBottom: '70px', textAlign: "center" }}>Ai 면접관과 함께하는 실전 면접 훈련
       <br />실제 대학 면접과 유사한 환경에서 맞춤형 질문을 받고, 즉각적인 피드백을 받아보세요</p>
 
-      <div className="center-both child-column-center" style={{ gap: '5px', margin: '0 auto' }}>
+      <div className="center-both child-column-center" style={{ gap: '10px', margin: '0 auto' }}>
         <div style={buttonStyle}>생기부 기반 맞춤 질문</div>
-        <h2 className="title-24-bold" style={{ color: 'var(--primary-60)', textAlign:"center", margin:"0px"}}>+</h2>
         <div style={buttonStyle}>다양한 성격의 면접관</div>
-        <h2 className="title-24-bold" style={{ color: 'var(--primary-60)', textAlign:"center", margin:"0px"}}>+</h2>
         <div style={buttonStyle}>AI 분석</div>
       </div>
 
