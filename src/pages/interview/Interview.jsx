0693c732bc6282ddd424bfa5008c952c6561a828
I-Chanwoo <chanuel0110@gmail.com>
2025-09-16 17:36:44 +0900

    Chore: 로딩 모달 수정

--- src/pages/interview/Interview.jsx
+++ src/pages/interview/Interview.jsx
@@ -257,7 +257,7 @@ function Interview({stream}) {
       <div className='side-margin'></div>
       <div className='interview-container'>
         <div className='loading' style={{ display: showLoading? "flex" : "none" }}>
-          <h4 className='title-40-bold' style={{ display: !isUploading ? "flex" : "none", color: "var(--background-color)", marginTop: "100px", textAlign: "center", marginBottom:"10px"}}>
+          <h4 className='title-40-bold' style={{ display: (timeLeft>0) ? "flex" : "none", color: "var(--background-color)", marginTop: "100px", textAlign: "center", marginBottom:"10px"}}>
             00:{timeLeft === 10 ? timeLeft : `0${timeLeft}`}
           </h4>
           <h4 className='subtitle-20-medium' style={{ color: "var(--background-color)", marginTop: "0px", textAlign: "center" }}>
