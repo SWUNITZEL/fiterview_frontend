20081b147c5c67799101593b1c5928f5f6d38dea
I-Chanwoo <chanuel0110@gmail.com>
2025-12-14 22:35:34 +0900

    Feat: 리포트 페이지 추가

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 import Join from "./pages/join/Join.jsx";
+import Report from "./pages/learning/Report.jsx";
 import Article from "./pages/article/Article.jsx";
 import ArticleDetail from "./pages/article/ArticleDetail.jsx";
 import { PATH } from "./config/paths";
@@ -15,6 +16,7 @@ const RouterConfig = () => {
       <Route path={PATH.MAIN} element={<Home />} />
       <Route path={PATH.LOGIN} element={<Login />} />
       <Route path={PATH.JOIN} element={<Join />} />
+      <Route path={PATH.LEARNING_REPORT} element={<Report />} />
       <Route path={PATH.ARTICLE} element={<Article />} />
       <Route path={PATH.ARTICLE_DETAIL} element={<ArticleDetail />} />
     </Routes>
