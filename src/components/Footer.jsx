4a459ac3ea90f17627a6f19585d6b692c58a5592
I-Chanwoo <chanuel0110@gmail.com>
2025-12-14 22:24:10 +0900

    Chore: Footer 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,19 +7,22 @@ const Footer = () => {
           style={{
             position:"relative",
             width: "100%",
-            backgroundColor: "var(--nuetral-20)",
-            height: "312px",
+            backgroundColor: "var(--color-gray-050)",
+            height: "416px",
             overflow: "hidden",
             display: "flex",
             flexDirection: "column",
             alignItems: "flex-start",
-            padding:"70px 240px",
-            color:"var(--nuetral-60)"
+            padding:"100px 96px",
+            color:"var(--color-gray-700)"
           }}
         >
         <img style={{ height:"27px"}} src="/images/default/logo_mono.png" alt="Logo"/>
-        <p>Copyright ⓒ 2025 Fiterview All right reserved | GITHUB: https://github.com/SWUNITZEL</p>
-        <p style={{position: "absolute", bottom: "70px", margin:"0px"}}>서울여자대학교 | 소프트웨어융합학과 | 이찬우 김하은 서영은 염정 임효진 </p>
+        <p style={{ marginTop:"24px", marginBottom: "8px"}}>주소: 01797 서울시 노원구 화랑로 621, 서울여자대학교</p>
+        <p style={{ marginTop:"0px", marginBottom: "8px"}}>대표이사: 000 | 사업자등록번호: 000-00-00000 | 통신판매신고번호: 제 2025-서울-00000호</p>
+        <p style={{ marginTop:"0px", marginBottom: "8px"}}>전화: 000-0000-0000 | 메일: 0000@email.com</p>
+        <p style={{ marginTop:"24px", marginBottom: "24px"}}>공지사항 | 이용약관 | 개인정보처리방침 | 청소년보호정책 | 자주묻는질문</p>
+        <p style={{ marginBottom: "100px"}}>Copyright © 2025 Nexture. All rights reserved.</p>
       </Container>
     );
 
