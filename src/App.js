4a3bc279c7bbfa96c22ea375a4c1b8f55f0867c0
cwlee <chanuel0110@gmail.com>
2025-12-15 14:20:35 +0900

    Feat: 배포용 진입경로 수정

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <ThemeProvider theme={MuiTheme}>
         <CssBaseline />
         <UserProvider>
-          <Router>
+          <Router basename="/nexture-frontend">
             <RouterConfig />
           </Router>
         </UserProvider>
