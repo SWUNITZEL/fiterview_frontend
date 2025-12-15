9b648329dc18bbd7186e9991fa7f2a2f075a6cdb
Han <gkscodud0527@gmail.com>
2025-12-15 18:23:58 +0900

    Feat: 학습 목록 페이지 구현

---
+++ src/data/learningData.js
@@ -0,0 +1,41 @@
+export const learningList = [
+  {
+    id: 1,
+    title: "소공녀 세라",
+    items: [
+      { id: 1, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+      { id: 2, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+      { id: 3, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+    ],
+  },
+  {
+    id: 2,
+    title: "신데렐라",
+    items: [
+      { id: 1, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+      { id: 2, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+    ],
+  },
+  {
+    id: 3,
+    title: "백설공주",
+    items: [
+      { id: 1, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+    ],
+  },
+  {
+    id: 4,
+    title: "아기돼지 삼형제",
+    items: [
+      { id: 1, chapter: "챕터명", progress: "000%", action: "보고서 보기" },
+      { id: 2, chapter: "챕터명", progress: "000%", action: "학습 불가" },
+    ],
+  },
+  {
+    id: 5,
+    title: "알프스 소녀 하이디",
+    items: [
+      { id: 1, chapter: "챕터명", progress: "000%", action: "수강하기" },
+    ],
+  },
+];
--- src/pages/learning/Learning.jsx
+++ src/pages/learning/Learning.jsx
@@ -1,12 +1,22 @@
 import { Container, Grid, Card, CardContent, Typography, Box } from '@mui/material';
+
 import { useNavigate } from 'react-router-dom';
 
-import NavbarComponent from '../../components/Navbar'
+import NavbarComponent from '../../components/Navbar';
+import { PATH } from "../../config/paths";
 import Footer from '../../components/Footer';
+import MainContainer from '../../components/MainContainer';
+
+import { learningList } from '../../data/learningData';
 
 
 const LearningMain = () => {
+
   const navigate = useNavigate();
+  
+  const handleCardClick = (titleId) => {
+    navigate(`${PATH.LEARNING}/${titleId}`);
+  };
 
   return (
     <Container
@@ -23,48 +33,114 @@ const LearningMain = () => {
     >
       <NavbarComponent />
 
-      <Container maxWidth="lg" style={{ marginTop: "120px", marginBottom: "60px" }}>
-        <Typography variant="h6" fontWeight="bold" gutterBottom>
+      <MainContainer>
+        <Typography variant="h1" fontWeight={700} fontSize={"28px"} marginBottom={"16px"}>
           로드맵
         </Typography>
 
-        <Box sx={{width : "232px"}}>
-          <Box
-            sx = {{
-              backgroundColor: "#333333",
-              color : "#ffffff",
-              padding : "8px",
-              borderRadius : 2,
-              marginBottom : "16px"
-            }}>
-            <Typography variant="h6" fontWeight="bold" align="center">
-              Titleㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
-            </Typography> 
-          </Box>
-
-          <Card 
-              sx={{ 
-                cursor: "pointer",
-                borderRadius: 2,
-                boxShadow: 3
-                // "&:hover": { boxShadow: 6, transform: "translateY(-4px)", transition: "0.2s" }
+        <Box
+          sx = {{
+            display: "grid",
+            gridTemplateColumns: {
+              xs: "1fr",
+              sm: "repeat(2, 1fr)",
+              md: "repeat(5, 1fr)"
+            },
+            columnGap: 3,
+          }}>
+          {learningList.map((column) => (
+            <Box
+              key={column.id}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "12px"
               }}
             >
-              <CardContent>
-                  <Typography variant="h6" fontWeight="bold">
-                    챕터명
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    퍼센테이지
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    박스및아이콘
-                  </Typography>
-                </CardContent>
-            </Card>
+              {/* 책 제목 */}
+              <Box
+                sx = {{
+                  backgroundColor: "var(--color-blue-500)",
+                  color: "#ffffff",
+                  padding : "8px 16px",
+                  borderRadius : "8px",
+                  marginBottom : "4px",
+                  height: "72px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center"
+                }}>
+                <Typography 
+                  fontWeight={700}
+                  fontSize={"18px"}
+                  lineHeight={"28px"}
+                  sx={{
+                    display: "-webkit-box",
+                    WebkitLineClamp: 2,
+                    webKitBoxOrient: "vertical",
+                    overflow: "hidden",
+                    maxHeight: "56px"
+                  }}>
+                    {column.title}
+                </Typography>
+              </Box>
+
+              {/* 카드 리스트 */}
+              {column.items.map((item) =>
+                <Card
+                  onClick={
+                    item.action === "학습 불가" ? undefined
+                    : () => handleCardClick(column.id)
+                  }
+
+                  key={item.id}
+                  sx={{ 
+                    boxShadow: "none",
+                    cursor: item.action === "학습 불가" ? "default" : "pointer",
+                    borderRadius: 2,
+                    backgroundColor: "var(--color-gray-100)",
+                    ...(item.action !== "학습 불가" && {
+                      "&:hover": { backgroundColor: "var(--color-gray-200)", transition: "0.2s" }
+                    })
+                    }}>
+                    <CardContent 
+                      sx = {{
+                        p: 2,
+                        "&:last-child": { pb: 2 }
+                      }}>
+                      <Typography variant="h2" fontWeight={700} fontSize={"18px"} color="var(--color-gray-900)" lineHeight="28px">
+                        {item.chapter}
+                      </Typography>
+
+                      <Typography variant="body2" fontWeight={400} fontSize={"16px"} color="var(--color-blue-500)" lineHeight={"24px"} marginBottom={"4px"}>
+                        {item.progress}
+                      </Typography>
+                    
+                      <Box 
+                        sx = {{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "space-between"
+                        }}>
+                          <Typography variant="body2" fontWeight={400} fontSize={"18px"} lineHeight={"28px"} 
+                          color={
+                            item.action === "학습 불가" 
+                            ? "var(--color-gray-400)" 
+                            : "var(--color-gray-700)"
+                          }>
+                            {item.action}
+                          </Typography>
+                          <Box component="img" src="" alt="" sx = {{ width: 20, height: 20 }} />
+                      </Box>
+                    </CardContent>
+                </Card>
+              )}
+            </Box>
+          ))}
         </Box>
 
-      </Container>
+      </MainContainer>
 
       <Footer />
     </Container>
